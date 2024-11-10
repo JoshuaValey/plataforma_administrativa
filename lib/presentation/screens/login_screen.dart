b9bc4f5440f4a26a312f6353238bde7d15d5a1c3
sebastianMLMJ <milianmejiaedgarsebastian@yahoo.com>
2024-11-10 14:52:43 -0600

    login

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:plataforma_administrativa/Repository/usuario.dart';
+import 'package:plataforma_administrativa/api_service.dart';
 import 'package:plataforma_administrativa/presentation/widgets/green_button.dart';
+import 'package:plataforma_administrativa/singleton.dart';
 
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  // Controladores para los campos de texto
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Limpiar los controladores cuando el widget se destruya
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    ApiService apiService = ApiService(url: Singleton.linkApiService);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Plataforma Limpia y Verde'),
+        leading: IconButton(
+          icon: const Icon(Icons.logout),
+          color: Colors.white,
+          onPressed: () {
+            // Acción al presionar el icono de logout
+          },
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -27,8 +56,9 @@ class LoginScreen extends StatelessWidget {
                   const SizedBox(height: 50),
                   ConstrainedBox(
                     constraints: const BoxConstraints(maxWidth: 400),
-                    child: const TextField(
-                      decoration: InputDecoration(
+                    child: TextField(
+                      controller: _usernameController, // Asignar controlador
+                      decoration: const InputDecoration(
                         labelText: 'Usuario',
                         border: OutlineInputBorder(),
                       ),
@@ -37,8 +67,9 @@ class LoginScreen extends StatelessWidget {
                   const SizedBox(height: 20),
                   ConstrainedBox(
                     constraints: const BoxConstraints(maxWidth: 400),
-                    child: const TextField(
-                      decoration: InputDecoration(
+                    child: TextField(
+                      controller: _passwordController, // Asignar controlador
+                      decoration: const InputDecoration(
                         labelText: 'Contraseña',
                         border: OutlineInputBorder(),
                       ),
@@ -47,11 +78,38 @@ class LoginScreen extends StatelessWidget {
                   ),
                   const SizedBox(height: 20),
                   GreenButton(
-                      label: 'Iniciar Sesión',
-                      onPressed: () {
-                        
-                        Navigator.pushNamed(context, '/main_screen');
-                      })
+                    label: 'Iniciar Sesión',
+                    onPressed: () {
+                      // Obtener los valores de los campos de texto
+                     
+                      String usuario  =  _usernameController.text;
+                      String contrasenia = _passwordController.text;
+
+                      if (usuario.isEmpty || contrasenia.isEmpty) {
+                        // Validación simple
+                        //Singleton.showToast('Por favor ingrese usuario y contraseña');
+                        return;
+                      }
+
+                      Usuario usuarioLogin = Usuario(
+                      nombreUsuario: usuario,
+                      contrasenia: contrasenia,
+                      rol: "Admin",
+                      );
+
+                      // Llamar a la API o hacer alguna acción con los datos
+                      apiService.login(usuarioLogin, '/usuario/iniciarsesion').then((response) {
+                        if (response.rol == "Admin") {
+                          //Singleton.showToast('Bienvenido');
+                          Navigator.pushNamed(context, '/main_screen');
+                        } else {
+                          //Singleton.showToast('Credenciales incorrectas');
+                        }
+                      }).catchError((e) {
+                        //Singleton.showToast('Error al intentar iniciar sesión');
+                      });
+                    },
+                  ),
                 ],
               ),
             ),
